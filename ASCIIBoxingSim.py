31312aa1a604d84301d6b50bb819f1c2cfb63d7b
Christopher Vu <christophervu4@gmail.com>
2024-11-25 15:14:19 -0800

    ASCII functionality, resting and jab

--- ASCIIBoxingSim.py
+++ ASCIIBoxingSim.py
@@ -1,12 +1,14 @@
 import time
 import random
 import itertools
+from boxingascii import frames
 
 combo_chance = 0.1
 punch_chance = 0.3
 reg_interval = 1
 combo_interval = 1
-contact_delay = 0.6
+contact_delay = 0.8
+starting_delay = 3
 
 rel_punch_freq = [(1, 10), (2, 3), (3, 4), ("OH", 3)]
 punch_selection = list(itertools.chain.from_iterable([[punch for i in range(freq)] for punch, freq in rel_punch_freq]))
@@ -14,14 +16,19 @@ punch_selection = list(itertools.chain.from_iterable([[punch for i in range(freq
 Combos = [(1, 2, 3), (1, 2), (1, 1, 2), (1, 2, "OH", 3), (1, "OH"), (2, 3), (1, 2, 5), (1, 2, "OH", 6)]
 
 def show_punch(punch, interval):
-    print(str(punch))
-    time.sleep(contact_delay)
-    print("*")
+    for frame in frames[str(punch)]:
+        print(frame)
+        time.sleep(contact_delay / len(frames[str(punch)]))
     time.sleep(interval-contact_delay)
 
 print("\nBEGINNING SIMULATION\n")
 print("<---------------->\n1=Jab\n2=Cross\n3=Lead Hook\n5=Lead Uppercut\n6=Rear Uppercut\nOH=Overhand Right\n<---------------->\n")
 print(f"Punch number will show {str(contact_delay)} seconds before landing.\nThe asterisk (*) means the punch conntected.")
+input("Press Enter to begin.")
+print("Simulation will start in...")
+for i in range(starting_delay):
+    print(f"{starting_delay-i}...")
+
 while True:
     if random.random() > (1-combo_chance):
         for punch in random.choice(Combos):
@@ -29,4 +36,6 @@ while True:
     elif random.random() > (1-punch_chance):
         show_punch(random.choice(punch_selection), reg_interval)
     else:
-        time.sleep(reg_interval)
+        for frame in frames["resting"]:
+            print(frame)
+            time.sleep(reg_interval/len(frames["resting"]))
