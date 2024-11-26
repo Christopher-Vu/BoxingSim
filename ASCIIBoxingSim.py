2a574d8bf6bb8ca2edd2ba35ec8d4d08bc19feed
Christopher Vu <christophervu4@gmail.com>
2024-11-25 16:32:04 -0800

    Jab and Cross Visibility adjustments

--- ASCIIBoxingSim.py
+++ ASCIIBoxingSim.py
@@ -11,9 +11,10 @@ contact_delay = 0.8
 starting_delay = 3
 
 rel_punch_freq = [(1, 10), (2, 3), (3, 4), ("OH", 3)]
+rel_freq_sum = sum([rel_freq for punch, rel_freq in rel_punch_freq])
 punch_selection = list(itertools.chain.from_iterable([[punch for i in range(freq)] for punch, freq in rel_punch_freq]))
 
-Combos = [(1, 2, 3), (1, 2), (1, 1, 2), (1, 2, "OH", 3), (1, "OH"), (2, 3), (1, 2, 5), (1, 2, "OH", 6)]
+combos = [(1, 2, 3), (1, 2), (1, 1, 2), (1, 2, "OH", 3), (1, "OH"), (2, 3), (1, 2, 5), (1, 2, "OH", 6)]
 
 def show_punch(punch, interval):
     for frame in frames[str(punch)]:
@@ -21,9 +22,18 @@ def show_punch(punch, interval):
         time.sleep(contact_delay / len(frames[str(punch)]))
     time.sleep(interval-contact_delay)
 
-print("\nBEGINNING SIMULATION\n")
-print("<---------------->\n1=Jab\n2=Cross\n3=Lead Hook\n5=Lead Uppercut\n6=Rear Uppercut\nOH=Overhand Right\n<---------------->\n")
-print(f"Punch number will show {str(contact_delay)} seconds before landing.\nThe asterisk (*) means the punch conntected.")
+print("BOXING SIMULATION\n")
+print("<--------PUNCH NUMBERS-------->\n1=Jab\n2=Cross\n3=Lead Hook\n5=Lead Uppercut\n6=Rear Uppercut\nOH=Overhand Right\n<---------------------------->\n")
+print(f"<--------SETTINGS INFO-------->\nInterval/Combo Interval: {str(reg_interval)}s, {str(combo_interval)}s")
+print(f"Punch/Combo Chance: {str(punch_chance * 100)}%, {str(combo_chance * 100)}%\nContact Delay: {str(contact_delay)}s\n<---------------------------->\n")
+print(f"<-RELATIVE PUNCH FREQUENCIES AND COMBOS->")
+print("Current Combos:")
+for combo in combos:
+    print(combo)
+print("Relative Punch Frequencies:")
+for pair in rel_punch_freq:
+    print(f"{pair[0]} : {pair[1]/rel_freq_sum*100}%")
+print("<--------------------------------------->\n")
 input("Press Enter to begin.")
 print("Simulation will start in...")
 for i in range(starting_delay):
@@ -31,7 +41,7 @@ for i in range(starting_delay):
 
 while True:
     if random.random() > (1-combo_chance):
-        for punch in random.choice(Combos):
+        for punch in random.choice(combos):
             show_punch(punch, combo_interval)
     elif random.random() > (1-punch_chance):
         show_punch(random.choice(punch_selection), reg_interval)
